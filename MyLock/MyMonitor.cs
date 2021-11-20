56f1d894fc650aa10c0639dc22cacc92122ab42a
1039270526@qq.com <1039270526@qq.com>
2021-11-20 15:52:30 +0800

    混合锁

---
+++ MyLock/MyMonitor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyLock
+{
+    /// <summary>
+    /// 混合锁Monitor
+    /// </summary>
+    public class MyMonitor
+    {
+        private static readonly object _lockObj = new object();
+        public static int Count1;
+        public static int Count2;
+        /// <summary>
+        /// Monitor
+        /// </summary>
+        public static void Show1()
+        {
+            bool lockTaken = false;
+            try
+            {
+                // Monitor是静态类
+                Monitor.Enter(_lockObj, ref lockTaken); // 获取锁
+
+                #region 锁保护区域
+                Count1++;
+                Count2++;
+                #endregion
+            }
+            finally
+            {
+                if(lockTaken)
+                    Monitor.Exit(_lockObj); // 释放锁
+            }
+        }
+        /// <summary>
+        /// Lock是Monitor的语法糖
+        /// </summary>
+        public static void Show2()
+        {
+            lock (_lockObj)
+            {
+                #region 锁保护区域
+                Count1++;
+                Count2++;
+                #endregion
+            }
+        }
+
+        /// <summary>
+        /// lock(this)测试
+        /// 每次线程产生一个新的this实例，那这个this只会在当前线程有效
+        /// 如果在外部锁住this，一直不释放，就会造成线程获取不了锁
+        /// </summary>
+        public void Show3()
+        {
+            lock (this)
+            {
+                Count1++;
+                Count2++;
+            }
+        }
+        /// <summary>
+        /// Lock(string)测试
+        /// </summary>
+        public void Show4()
+        {
+            lock ("lockStr")
+            {
+                Count1++;
+                Count2++;
+            }
+        }
+    }
+}
--- MyLock/Program.cs
+++ MyLock/Program.cs
@@ -33,6 +33,86 @@ namespace MyLock
 
             #endregion
 
+            #region 混合锁
+
+            // Monitor实现
+            //TestLock(MyMonitor.Show1);
+            //Console.WriteLine(MyMonitor.Count1);
+            //Console.WriteLine(MyMonitor.Count2);
+
+            // Lock实现
+            //TestLock(MyMonitor.Show2);
+            //Console.WriteLine(MyMonitor.Count1);
+            //Console.WriteLine(MyMonitor.Count2);
+
+            #region Lock(this)测试
+
+            {
+                //List<Task> tasks = new List<Task>();
+                //for (int i = 0; i < 200; i++)
+                //{
+                //    tasks.Add(Task.Run(() => {
+                //        // 每次线程产生一个新的this实例，那这个this只会在当前线程有效，达不到预期效果
+                //        MyMonitor obj = new MyMonitor();
+                //        obj.Show3();
+                //    }));
+                //}
+                //Task.WaitAll(tasks.ToArray());
+                //Console.WriteLine(MyMonitor.Count1);
+                //Console.WriteLine(MyMonitor.Count2);
+            }
+            {
+                //List<Task> tasks = new List<Task>();
+                //MyMonitor obj = new MyMonitor();
+                //for (int i = 0; i < 200; i++)
+                //{
+                //    tasks.Add(Task.Run(() => {
+                //        obj.Show3();
+                //    }));
+                //}
+                //Task.WhenAll(tasks.ToArray()).ContinueWith(t => {
+                //    Console.WriteLine(MyMonitor.Count1);
+                //    Console.WriteLine(MyMonitor.Count2);
+                //});
+                //// 这里锁住obj，就相当于锁住Show3方法中的this，这个lock先执行，那么Show3的lLock(this)就会一直等待锁的释放
+                //// 一直不释放，则程序就会一直卡住
+                //lock (obj)
+                //{
+                //    Thread.Sleep(int.MaxValue);
+                //}
+            }
+
+            #endregion
+
+            #region Lock字符串
+
+            //List<Task> tasks = new List<Task>();
+            //MyMonitor obj = new MyMonitor();
+            //for (int i = 0; i < 200; i++)
+            //{
+            //    tasks.Add(Task.Run(() =>
+            //    {
+            //        obj.Show4();
+            //    }));
+            //}
+            //Task.WhenAll(tasks.ToArray()).ContinueWith(t =>
+            //{
+            //    Console.WriteLine(MyMonitor.Count1);
+            //    Console.WriteLine(MyMonitor.Count2);
+            //});
+            //// 这里lock的字符串和Show4方法中lock的字符串相同，由于字符串的享元特性，两个地方其实是lock的同一个对象
+            //// 这里一直不释放，那Show4就会一直等待
+            //var _lockObj = "lockStr";
+            //lock (_lockObj)
+            //{
+            //    Thread.Sleep(int.MaxValue);
+            //}
+
+            #endregion
+
+            #endregion
+
+
             Console.ReadKey();
         }
 
